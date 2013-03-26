d7d411fe080254860425afd795afcdc3d0541ee4
root <root@AY1212191030153a51387>
2013-03-26 23:48:30 +0800

    remote files

--- dragon/src/platform/platform.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PLATFORM_HH_
-#define _PLATFORM_HH_
-
-// random 
-int random();
-
-
-
-#endif
--- dragon/src/platform/platform_linux.cc
+++
@@ -1,9 +0,0 @@
-#ifndef _PLATFORM_LINUX_XX_
-#define _PLATFORM_LINUX_XX_
-
-int random(){
-    return 23;
-}
-
-
-#endif
