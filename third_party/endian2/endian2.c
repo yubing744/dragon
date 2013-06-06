b5b2fc228572805c9cfe99ec00ed18e390d7fcea
wcw <yubing744@163.com>
2013-06-07 07:34:10 +0800

    add depend lib endian

---
+++ third_party/endian2/endian2.c
@@ -0,0 +1,95 @@
+#include "endian2.h"
+
+
+static __inline int
+__endian2(const char *__restrict fmt, void *p)
+{
+	const char *i;
+	for (i = fmt; *i != '\0'; i++) {
+#define _swpint(T) { \
+		__uint##T##_t *n = p; \
+		*n = bswap##T(*n); \
+		p += sizeof(__uint##T##_t); \
+		break; \
+	}
+#define _noswp(n) { \
+		p += n; \
+		break; \
+	}
+		switch (*i) {
+		case 'h':
+			_swpint(16);
+		case 'l':
+		case 'f':
+			_swpint(32);
+		case 'q':
+		case 'd':
+			_swpint(64);
+		case '.':
+		case 'c':
+			_noswp(1);
+		default : return -1;
+		}
+#undef _swpint
+#undef _noswp
+	}
+	return i - fmt;
+}
+
+/*
+ * Reference:
+ * http://stackoverflow.com/questions/1001307/detecting-endianness-programmatically-in-a-c-program
+ */
+#ifndef is_big_endian
+
+static __inline int
+is_big_endian(void)
+{
+	union {
+		__uint32_t i;
+		char c[4];
+	} bint = {0x01020304};
+	return bint.c[0] == 1;
+}
+
+#endif
+
+/*
+ * converts any endianness to host
+ * returns -1 when the format is wrong,
+ *          0 when no conversion is needed,
+ *          positive value == len(fmt) - 1 (w/o the leading '>'/'<').
+ */
+int
+endian2h(const char *__restrict fmt, void *p)
+{
+	if (is_big_endian()) {
+		if (fmt[0] == '>') return 0;
+		if (fmt[0] != '<') return -1;
+	} else {
+		if (fmt[0] == '<') return 0;
+		if (fmt[0] != '>') return -1;
+	}
+	return __endian2(fmt + 1, p);
+}
+
+/*
+ * converts host endianness to any
+ * yes, it's identical to endian2h
+ */
+int
+h2endian(const char *__restrict fmt, void *p) {
+	return endian2h(fmt, p);
+}
+
+/*
+ * reverses any endianness
+ * returns -1 when the format is wrong,
+ *		  other value == len(fmt).
+ */
+int
+r_endian(const char *__restrict fmt, void *p)
+{
+	return __endian2(fmt, p);
+}
+
---
+++ third_party/endian2/endian2.h
@@ -0,0 +1,64 @@
+#ifndef _ENDIAN2_H
+#define _ENDIAN2_H
+
+
+#include <sys/types.h>
+
+int
+endian2h(const char *__restrict __fmt, void *__src);
+
+int
+h2endian(const char *__restrict __fmt, void *__src);
+
+int
+r_endian(const char *__restrict __fmt, void *__src);
+
+/* Reference: LLVM compiler-rt/trunk/lib/endianness.h */
+
+#if defined(__SVR4) && defined(__sun)
+#include <sys/byteorder.h>
+#define is_big_endian() (_BYTE_ORDER == _BIG_ENDIAN)
+#endif /* Solaris and AuroraUX. */
+
+#if defined(__FreeBSD__) || defined(__NetBSD__) || \
+	defined(__OpenBSD__) || defined(__DragonFly__)
+#include <sys/endian.h>
+#define is_big_endian() (_BYTE_ORDER == _BIG_ENDIAN)
+#endif /* *BSD */
+
+#if defined(__APPLE__) && defined(__MACH__) || defined(__ellcc__ )
+#define __BIG_ENDIAN__ 1
+#define is_big_endian() (__BIG_ENDIAN__)
+#endif /* Mac OSX */
+
+#if defined(__linux__)
+#include <endian.h>
+#define is_big_endian() (__BYTE_ORDER == __BIG_ENDIAN)
+#endif /* GNU/Linux */
+
+/* Reference: FreeBSD /usr/include/sys/endian.h */
+
+#ifndef bswap16
+
+#define __bswap64(x) \
+__extension__ ({ register __uint64_t __X = (x); \
+   __asm ("bswap %0" : "+r" (__X)); \
+   __X; })
+
+#define __bswap32(x) \
+__extension__ ({ register __uint32_t __X = (x); \
+   __asm ("bswap %0" : "+r" (__X)); \
+   __X; })
+
+static __inline __uint64_t
+bswap64(__uint64_t _x) { return (__bswap64(_x)); }
+
+static __inline __uint32_t
+bswap32(__uint32_t _x) { return (__bswap32(_x)); }
+
+static __inline __uint16_t
+bswap16(__uint16_t _x) { return (_x << 8 | _x >> 8); }
+
+#endif
+
+#endif
