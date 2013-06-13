4cfeb3935e918658fafc8aba4a704c01f3ed3cc8
wcw <yubing744@163.com>
2013-06-13 21:26:10 +0800

    add libunicode config.h

---
+++ third_party/libunicode-0.4/config.h
@@ -0,0 +1,18 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if system iconv() should be used.  */
+/* #undef UNICODE_USE_SYSTEM_ICONV */
+
+/* Define if you have <langinfo.h> */
+#define HAVE_LANGINFO_H 1
+
+/* Define if you have the memmove function.  */
+#define HAVE_MEMMOVE 1
+
+/* Name of package */
+#define PACKAGE "libunicode"
+
+/* Version number of package */
+#define VERSION "0.4"
+
