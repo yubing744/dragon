aa2517b30053fc5e519f19475bb0bbf68f7d7f9f
wcw <yubing744@163.com>
2014-04-21 08:15:20 +0800

    model

--- osgi/src/DllMain.cpp
+++ osgi/src/DllMain.cpp
@@ -1,11 +1,9 @@
 #include <dragon.h>
 
-BOOL APIENTRY DllMain(HANDLE hModule,DWORD dwReason,LPVOID lpReserved)
-{
-	if (dwReason == DLL_PROCESS_ATTACH)
-	{
-		if(LM::GetInstance()->getLibrary(L"dragon_osgi") == (P<Library>)null)
-		{
+BOOL APIENTRY DllMain(HANDLE hModule,DWORD dwReason,LPVOID lpReserved) {
+
+	if (dwReason == DLL_PROCESS_ATTACH) {
+		if(LM::GetInstance()->getLibrary(L"dragon_osgi") == (P<Library>)null) {
 			P<Library> syslib=new DllLibrary((HMODULE)hModule);
 			LM::GetInstance()->registLibrary(L"dragon_osgi",syslib);
 		}
